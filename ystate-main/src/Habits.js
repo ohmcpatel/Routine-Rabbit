f738e1fce00e35049442848b999a22fef0e2dc45
Ohm Patel <106202323+ohmcpatel@users.noreply.github.com>
2023-01-22 15:06:48 -0500

    First Commit

---
+++ ystate-main/src/Habits.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import "./Habits.css"
+import Navbar from './Navbar';
+
+function Habits( {send}) {
+    return (
+        <div>
+            <Navbar />
+            <h1>Habits</h1>
+        </div>
+    );
+}
+
+export default Habits;
